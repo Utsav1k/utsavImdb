28d682c113d10c92f4dc76d68c37edec455cad60
Utsav Khandelwal <35996599+Utsav1k@users.noreply.github.com>
2021-07-06 20:42:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
   }));
 app.use(Cookies());
 app.use((req,res,next) =>{
-	//res.header("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow-Origin","*");
 	res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Credentials", true);
 	next();
