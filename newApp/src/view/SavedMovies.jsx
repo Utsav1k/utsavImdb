1386ebfdf4ee7a08e15107193409160f9745de92
Utsav1k <utsav.khandelwal97@gmail.com>
2021-07-10 16:59:08 +0530

    change

--- newApp/src/view/SavedMovies.jsx
+++ newApp/src/view/SavedMovies.jsx
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {Card,Button,Dimmer,Loader, Message} from 'semantic-ui-react';
+import {Card,Button,Dimmer,Loader, Message,Input,Segment} from 'semantic-ui-react';
 import Route from '../routes/route.js';
 import ImdbHomeController from '../controllers/imdb/imdb.js';
 import ImdbMovieDetails from './imdb/imdbMovieDetails.jsx';
@@ -7,25 +7,39 @@ import ImdbMovieDetails from './imdb/imdbMovieDetails.jsx';
 class SavedMovies extends React.Component{
     constructor(props){
         super(props);
-        this.state={movieList:[],selectedMovieDetailsModelOpen:false,selectedMovieDetails:null,loader:false}
+        this.state={movieListCopy:[],movieList:[],selectedMovieDetailsModelOpen:false,selectedMovieDetails:null,loader:false}
     }
     componentDidMount(){
         (async () => {
             try {
               const res = await Route.Imdb.getSavedMovies(  ) ;
-              this.setState({movieList:res.body});
+              this.setState({movieList:res.body,movieListCopy:res.body});
               // console.log(res);
             } catch (err) {
               // console.log(err);
             }
           })();
     }
-
+    searchSave(e){
+        let temp=[];
+        if(e.target.value==''){
+            this.setState({movieList:this.state.movieListCopy});
+        }
+        else{
+            for(let i=0;i<this.state.movieListCopy.length;i++){
+                if(this.state.movieListCopy[i].movie.title.toLowerCase().includes(e.target.value.toLowerCase())){
+                    temp.push(this.state.movieListCopy[i]);
+                }
+            }
+            this.setState({movieList:temp});
+        }
+        
+    }
     render(){
         return(
-            <div>
+            <div style={{overflow:'auto'}}>
                 <Dimmer active={this.state.loader}><Loader active={this.state.loader} >Loading...</Loader></Dimmer>
-
+                <Segment style={{overflow:'auto'}}><Input style={{float:'right'}} onChange={this.searchSave.bind(this)} placeholder="search ..." /> </Segment>
                 <Card.Group style={{overflow:'auto'}}>
                     {this.state.movieList!=undefined && this.state.movieList.length>0 ?this.state.movieList.map((item1,index1) =>{
                         return <Card style={{overflow:'auto'}}
